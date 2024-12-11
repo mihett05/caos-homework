952ba981fbdf363136913ef6d72e488c2639af52
mihett05 <mihett05@yandex.ru>
2024-12-11 17:44:44 +0500

    Updated run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,23 @@
 
 for i in $(dirname $(find . -type f -iname "Makefile"))
 do
-    make -C $i
-    make test -C $i
+    passed=0
+    total=0
+
+    make -s -C $i > /dev/null 2>&1
+    while read -r line; do
+        if [[ $line == Test* ]]; then
+            ((total++))
+        fi
+
+        if [[ $line == *0 ]]; then
+            ((passed++))
+        fi
+    done < <(make test -s -C $i 2> /dev/null)
+
+    if [ $passed -eq $total ]; then
+        echo -e "$(realpath -s --relative-to=$(pwd) $i): \033[0;32mPASS\033[0m [$passed/$total]"
+    else
+        echo -e "$(realpath -s --relative-to=$(pwd) $i): \033[1;31mFAIL\033[0m [$passed/$total]"
+    fi
 done
